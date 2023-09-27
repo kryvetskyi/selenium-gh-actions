bfe4ecb0806e0964b2cb4b8f575f81f017cb7bc9
kryvetskyi <kryvetskyi@gmail.com>
2023-09-27 20:58:07 +0300

    fix for chrome version and chromedriver

--- conftest.py
+++ conftest.py
@@ -1,18 +1,27 @@
+from pathlib import Path
+
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-@pytest.fixture()       # to parametrize use @pytest.fixture(params=["chrome", "firefox", ...])
-def driver(request):    # and driver = request.param
+def get_chrome_path_binary():
+    project_root = Path(__file__).resolve().parent
+    chrome_executable_path = project_root / "binaries" / "chrome-mac-arm64" / "Google Chrome for Testing.app" / \
+                                            "Contents" / "MacOS" / "Google Chrome for Testing"
+    return str(chrome_executable_path)
+
+
+@pytest.fixture()
+def driver(request):
+    options = webdriver.ChromeOptions()
+    options.binary_location = get_chrome_path_binary()
+
     driver = request.config.getoption("--browser")
     if driver == "chrome":
-        driver = webdriver.Chrome()
-        driver.maximize_window()
+        driver = webdriver.Chrome(options=options)
     elif driver == "firefox":
         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
         driver.maximize_window()
