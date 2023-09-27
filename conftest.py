66f2a338b2ac18c6e20c118280a6e7a904d6d042
kryvetskyi <kryvetskyi@gmail.com>
2023-09-27 22:46:45 +0300

    added headless option

--- conftest.py
+++ conftest.py
@@ -10,22 +10,25 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 def get_chrome_path_binary():
     project_root = Path(__file__).resolve().parent
+
     if platform.system() == "Linux":
         chrome_executable_path = project_root / "binaries" / "chrome-linux64" / "chrome"
     else:
         chrome_executable_path = project_root / "binaries" / "chrome-mac-arm64" / "Google Chrome for Testing.app" / \
                                             "Contents" / "MacOS" / "Google Chrome for Testing"
-    print('PATH IS:', chrome_executable_path)
     return str(chrome_executable_path)
 
 
 @pytest.fixture()
 def driver(request):
+    headless = request.config.getoption("--headless")
+    driver = request.config.getoption("--browser")
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+
+    if headless:
+        options.add_argument("--headless")
     options.binary_location = get_chrome_path_binary()
 
-    driver = request.config.getoption("--browser")
     if driver == "chrome":
         driver = webdriver.Chrome(options=options)
     elif driver == "firefox":
@@ -42,3 +45,5 @@ def pytest_addoption(parser):
     parser.addoption(
         "--browser", action="store", default="chrome", help="Please provide correct browser"
     )
+    parser.addoption("--headless", action="store_true", help="Run the browser in headless mode")
+
